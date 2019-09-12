5171215277c590e901f4052d00a5773374c7f0d1
hanlinkhaing <hanlinkhaing300695@gmail.com>
2019-09-12 21:49:45 +0630

    Initial commit

---
+++ src/main/java/com/deltamate/demo/DeltaMateApplication.java
@@ -0,0 +1,13 @@
+package com.deltamate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaMateApplication.class, args);
+    }
+
+}
